5edd27932ef36fe4fc5c809b0f3219b5e9e43489
Carlos Alvarado <carlosalvaradom@icloud.com>
2023-03-16 14:20:20 -0500

    First Commit

---
+++ src/test/java/co/edu/ucatolica/primerparcial/PrimerparcialApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ucatolica.primerparcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerparcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
