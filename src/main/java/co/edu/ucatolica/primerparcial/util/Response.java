fd1522170a9e3cdae64e526f6aa893daf550d83d
Carlos Alvarado <carlosalvaradom@icloud.com>
2023-03-16 14:49:23 -0500

    Configuracion API y Util Response

---
+++ src/main/java/co/edu/ucatolica/primerparcial/util/Response.java
@@ -0,0 +1,20 @@
+package co.edu.ucatolica.hisclinic.util;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+@SuperBuilder
+@JsonInclude(JsonInclude.Include.ALWAYS)
+public class Response {
+    protected LocalDateTime timeStampo;
+    protected int statusCode;
+    protected HttpStatus status;
+    protected String message;
+    protected Map<?,?> data;
+}
